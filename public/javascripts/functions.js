7fde23b2c64a61114a585f5b8d08c92b54e035e2
Julian <julian@julians-pc.(none)>
2015-01-14 10:31:56 +0100

    kkhgjh

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -29,18 +29,18 @@ function drop(ev) {
 }
 
 function cellclicked(ev){
-    figurex=ev.target.dataset.x;
-    figurey=ev.target.dataset.y;
-    
+
     if(clickflag == 0){
-        $("input.InputFigurex").val(destx);
-        $("input.InputFigurey").val(9-desty);
+        figurex=ev.target.dataset.x;
+        figurey=ev.target.dataset.y;
+        $("input.InputFigurex").val(figurex);
+        $("input.InputFigurey").val(9-figurey);
         clickflag = 1;
     }else{
-        desty=figurey;
-        destx=figurex;
-        $("input.InputDestinationx").val(figurex);
-        $("input.InputDestinationy").val(9-figurey);
+        destx=ev.target.dataset.x;
+        desty=ev.target.dataset.y;
+        $("input.InputDestinationx").val(destx);
+        $("input.InputDestinationy").val(9-desty);
 
         var input = "input/" + figurey + "%20" + figurex + "%20" + desty + "%20" + destx
         $("#dynamicallyLoadableContent").load(input + " #dynamicallyLoadableContent");
